7a78f733eb416141b1cee4ed21847002ea72d934
Maureen Nyabuto <maureennyabuto27@gmail.com>
2023-02-23 05:04:27 -0800

    main file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
  *main - entry point
  *
  *Return: Always 0
  */
-int main(void)
+int _putchar(char c)
 {
-putchar("\"_putchar");
+_putchar("\"_putchar");
 return (0);
 }

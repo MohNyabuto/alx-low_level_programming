eff210b4cf3e9e8dbc5740145055a4113baa14b2
Maureen Nyabuto <maureennyabuto27@gmail.com>
2023-03-02 15:26:46 -0800

    task eight

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,22 @@
 #include "main.h"
 /**
- * leet - function that encodes a string into 1337
- * @s: string array
- * Return: string array
+ * leet - a function that encodes a string into 1337.
+ * @s: an input string to encode
+ * Return: An encode string
  */
-
 char *leet(char *s)
 {
-int i;
+int i = 0, j;
+char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
+str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
-for (i = 0; s[i] != '\0'; i++)
+while (s[i])
 {
-if (s[i] == 'a' || s[i] == 'A')
-s[i] = '4';
-if (s[i] == 'e' || s[i] == 'E')
-s[i] = '3';
-if (s[i] == 'o' || s[i] == 'O')
-s[i] = '0';
-if (s[i] == 't' || s[i] == 'T')
-s[i] = '7';
-if (s[i] == 'l' || s[i] == 'L')
-s[i] = '1';
+for (j = 0; j < 10; j++)
+if (s[i] == str[j])
+s[i] = subs[j];
+
+i++;
 }
 
 return (s);
